4157549d55df5948c879bef1631e629cfb3a1042
Andrew Delpha <andrew@scrible.com>
2023-05-01 23:47:23 -0500

    Got stuck in a loop.

--- src/app/services/photo-result-set.service.ts
+++ src/app/services/photo-result-set.service.ts
@@ -102,6 +102,7 @@ export class PhotoResultSetService {
       let pfd = this.photosByDayHash[day];
       if (pfd) {
         if (this.photosByDayList.indexOf(pfd) === this.photosByDayList.length - 1 && this.outlineNextOffsetDate) {
+          this.outlineNextOffsetDate = null; //wipe this out so we don't renter this until the next page is ready
           //We've loaded the last day, from the current outline results - load the next outline page
           this.fetchResultsOutline(this.outlineNextOffsetDate); //we'll just ignore the promise  and let this happen in the background
         }
@@ -289,6 +290,7 @@ export class PhotoResultSetService {
         //It's in the view area
         pfd.dateInViewRange = true;
         if (this.photosByDayList.length === idx + 1 && this.outlineNextOffsetDate) {
+          this.outlineNextOffsetDate = null; //wipe this out so we don't renter this until the next page is ready
           //We've loaded the last day, from the current outline results - load the next outline page
           this.fetchResultsOutline(this.outlineNextOffsetDate); //we'll just ignore the promise  and let this happen in the background
         }
